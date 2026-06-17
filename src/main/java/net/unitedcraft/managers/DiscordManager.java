67ba974002479b0ea08d624205d96b9ad59c5978
lucienskyeanderson-wq <lucienskyeanderson@gmail.com>
2026-06-16 20:36:54 -0700

    Add src/main/java/net/unitedcraft/managers/DiscordManager.java

---
+++ src/main/java/net/unitedcraft/managers/DiscordManager.java
@@ -0,0 +1,7 @@
+package net.unitedcraft.managers;
+
+// Placeholder for future Discord webhook integration
+// Will broadcast war declarations, election results, coups to a Discord channel
+public class DiscordManager {
+    // TODO: Add Discord webhook URL to config and broadcast key nation events
+}
